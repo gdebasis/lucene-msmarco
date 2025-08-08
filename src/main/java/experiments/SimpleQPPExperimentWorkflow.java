79692d199210bc5826293eb0c6717a447153f757
Debasis Ganguly <debforit@gmail.com>
2025-08-08 23:12:03 +0100

    a simple workflow added

---
+++ src/main/java/experiments/SimpleQPPExperimentWorkflow.java
@@ -0,0 +1,66 @@
+package experiments;
+
+import correlation.KendalCorrelation;
+import org.apache.lucene.search.TopDocs;
+import qpp.*;
+import qrels.Evaluator;
+import qrels.Metric;
+import retrieval.Constants;
+import retrieval.KNNRelModel;
+import retrieval.MsMarcoQuery;
+import retrieval.OneStepRetriever;
+import stochastic_qpp.QPPMetricBundle;
+import utils.IndexUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleQPPExperimentWorkflow {
+    static final int NUM_DOCS = 50;
+
+    public static void main(String[] args) throws Exception {
+        List<MsMarcoQuery> queries;
+        final String resFile = "pass_2019.queries.res";
+        OneStepRetriever retriever = new OneStepRetriever(Constants.QUERY_FILE_TEST, resFile, "english");
+
+        QPPMethod[] qppMethods = {
+                new NQCSpecificity(retriever.getSearcher()),
+//                new VariantSpecificity(
+//                        new NQCSpecificity(retriever.getSearcher()),
+//                        retriever.getSearcher(),
+//                        new KNNRelModel(Constants.QRELS_TRAIN, Constants.QUERY_FILE_TEST, false),
+//                        5, 0.2f
+//                ),
+                new OddsRatioSpecificity(retriever.getSearcher(), 0.6f),
+                //new WIGSpecificity(retriever.getSearcher()),
+                //new NQCCalibratedSpecificity(retriever.getSearcher(), 0.33f, 0.33f, 0.33f),
+                //new RSDSpecificity(new NQCSpecificity(retriever.getSearcher())),
+                //new UEFSpecificity(new NQCSpecificity(retriever.getSearcher()))
+        };
+
+        queries = retriever.getQueryList();
+        IndexUtils.init(retriever.getSearcher());
+
+        Evaluator evaluator = new Evaluator(Constants.QRELS_TEST, resFile, NUM_DOCS); // Metrics for top-100 (P@10 is still at 10)
+        List<Double> evaluatedMetricValues = new ArrayList<>();
+        for (MsMarcoQuery query: queries) {
+            evaluatedMetricValues.add(evaluator.compute(query.getId(), Metric.AP));
+        }
+
+        for (QPPMethod qppMethod: qppMethods) {
+            Map<String, TopDocs> topDocsMap = evaluator.getAllRetrievedResults().castToTopDocs();
+            List<Double> qppEstimates = new ArrayList<>();
+            for (MsMarcoQuery query : queries) {
+                qppEstimates.add(qppMethod.computeSpecificity(query, topDocsMap.get(query.getId()), 50));
+            }
+
+            double tau = new KendalCorrelation().correlation(
+                    qppEstimates.stream().mapToDouble(Double::doubleValue).toArray(),
+                    evaluatedMetricValues.stream().mapToDouble(Double::doubleValue).toArray()
+            );
+
+            System.out.println(String.format("model: %s, tau: %.4f", qppMethod.name(), tau));
+        }
+    }
+}
