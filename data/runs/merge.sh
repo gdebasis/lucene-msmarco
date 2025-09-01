047f4c4efcee67e8c0fe33acdfc6aad07240e0e7
Payel Santra <payelsantra@Payels-MacBook-Air.local>
2025-09-01 13:50:20 +0530

    added 1920 runs

---
+++ data/runs/merge.sh
@@ -0,0 +1,8 @@
+cat 2019/dense_qpp_another_calculation/BM25.2019.100.res 2020/dense_qpp_another_calculation/BM25.2020.100.res > 1920/dense_qpp_another_calculation/BM25.1920.100.res
+cat 2019/dense_qpp_another_calculation/cobert.e2e.100.res 2020/dense_qpp_another_calculation/colbert.e2e.100.res > 1920/dense_qpp_another_calculation/colbert.e2e.100.res
+cat 2019/dense_qpp_another_calculation/e5_dl_19.100.res 2020/dense_qpp_another_calculation/e5_dl_20.100.res > 1920/dense_qpp_another_calculation/e5_dl_1920.100.res
+cat 2019/dense_qpp_another_calculation/monot5.100.res 2020/dense_qpp_another_calculation/monot5.100.res > 1920/dense_qpp_another_calculation/monot5.100.res
+cat 2019/dense_qpp_another_calculation/prf_rank_beta05.2019.100.res 2020/dense_qpp_another_calculation/prf_rank_beta05.2020.100.res > 1920/dense_qpp_another_calculation/prf_rank_beta05.1920.100.res
+cat 2019/dense_qpp_another_calculation/prf_rerank_beta05.2019.100.res 2020/dense_qpp_another_calculation/prf_rerank_beta05.2020.100.res > 1920/dense_qpp_another_calculation/prf_rerank_beta05.1920.100.res
+cat 2019/dense_qpp_another_calculation/rm3.100.res 2020/dense_qpp_another_calculation/rm3.100.res > 1920/dense_qpp_another_calculation/rm3.100.res
+cat 2019/dense_qpp_another_calculation/splade.100.res 2020/dense_qpp_another_calculation/splade.100.res > 1920/dense_qpp_another_calculation/splade.100.res
